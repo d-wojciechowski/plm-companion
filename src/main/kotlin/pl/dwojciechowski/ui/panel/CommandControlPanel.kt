2d923221b5951a27f958dfcef149ae559145c4ef
Dominik Wojciechowski <Dominik.Wojciechowski@ttpsc.pl>
2020-01-31 09:50:47 +0100

    Panel update

--- src/main/kotlin/pl/dwojciechowski/ui/panel/CommandControlPanel.kt
+++ src/main/kotlin/pl/dwojciechowski/ui/panel/CommandControlPanel.kt
@@ -4,22 +4,23 @@ import com.intellij.icons.AllIcons
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.SimpleToolWindowPanel
-import com.intellij.ui.components.JBTextArea
 import pl.dwojciechowski.service.WncConnectorService
 import javax.swing.JButton
 import javax.swing.JPanel
+import javax.swing.JTextArea
 
-class CommandControlPanel(
-    private val project: Project
-) : SimpleToolWindowPanel(false, true) {
+class CommandControlPanel(project: Project) : SimpleToolWindowPanel(false, true) {
 
     private val commandService: WncConnectorService =
         ServiceManager.getService(project, WncConnectorService::class.java)
 
     lateinit var panel: JPanel
 
-    private lateinit var contentArea: JBTextArea
+    private lateinit var contentArea: JTextArea
     private lateinit var clearButton: JButton
+    private lateinit var autoScrollJButton: JButton
+
+    private var autoScroll = true
 
     init {
         this.add(panel)
@@ -27,9 +28,15 @@ class CommandControlPanel(
         clearButton.addActionListener { contentArea.text = "" }
         clearButton.icon = AllIcons.Actions.GC
 
+        autoScrollJButton.icon = AllIcons.General.AutoscrollFromSource
+        autoScrollJButton.addActionListener {
+            autoScroll = !autoScroll
+            autoScrollJButton.icon = if (autoScroll) AllIcons.General.AutoscrollFromSource else AllIcons.General.ZoomOut
+        }
+
         commandService.getOutputSubject().subscribe {
             contentArea.append(it + "\n")
-            contentArea.caretPosition = contentArea.document.length
+            if (autoScroll) contentArea.caretPosition = contentArea.document.length
         }
 
     }
