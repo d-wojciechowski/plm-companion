a29983cd62eb2babcee0660916fc9eae116c3471
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2020-05-09 14:05:48 +0200

    #11 Added pre run configurations for Load From File

---
+++ src/main/kotlin/pl/dwojciechowski/ui/component/RunConfigurationRepresentation.kt
@@ -0,0 +1,13 @@
+package pl.dwojciechowski.ui.component
+
+import com.intellij.execution.RunnerAndConfigurationSettings
+import javax.swing.Icon
+
+data class RunConfigurationRepresentation (
+    val uniqueId: String,
+    val displayName: String,
+    val icon: Icon,
+    val value: RunnerAndConfigurationSettings? = null
+) {
+    override fun toString() = displayName
+}
