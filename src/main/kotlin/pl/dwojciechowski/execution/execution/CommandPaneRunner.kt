d4ff94bc1cf42447f9311c6b1237f8172bf5a8ff
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2020-05-20 21:55:12 +0200

    changed compatibility

--- src/main/kotlin/pl/dwojciechowski/execution/execution/CommandPaneRunner.kt
+++ src/main/kotlin/pl/dwojciechowski/execution/execution/CommandPaneRunner.kt
@@ -6,9 +6,6 @@ import com.intellij.execution.configurations.RunnerSettings
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
-import com.intellij.openapi.wm.ToolWindow
-import com.intellij.openapi.wm.ToolWindowManager
-import com.intellij.ui.content.Content
 import pl.dwojciechowski.execution.config.RemoteCommandRunConfig
 
 class CommandPaneRunner : ProgramRunner<RunnerSettings> {
@@ -17,11 +14,7 @@ class CommandPaneRunner : ProgramRunner<RunnerSettings> {
 
     @Throws(ExecutionException::class)
     override fun execute(environment: ExecutionEnvironment) {
-        withCommandPane(environment) { toolWindow, content ->
-            val contentManager = toolWindow.contentManager
-            toolWindow.show()
-            contentManager.setSelectedContent(content, true, false)
-        }
+
         environment.state?.execute(environment.executor, this)
     }
 
@@ -33,13 +26,5 @@ class CommandPaneRunner : ProgramRunner<RunnerSettings> {
         return DefaultRunExecutor.EXECUTOR_ID == executorId && profile is RemoteCommandRunConfig
     }
 
-    private fun withCommandPane(environment: ExecutionEnvironment, method: (ToolWindow, Content) -> Unit) {
-        val instance = ToolWindowManager.getInstance(environment.project)
-        instance.getToolWindow("PLM Companion Log")?.let { toolWindow ->
-            val contentManager = toolWindow.contentManager
-            contentManager.getContent(2)?.let { content ->
-                method(toolWindow, content)
-            }
-        }
-    }
+
 }
--- src/main/kotlin/pl/dwojciechowski/execution/state/RemoteCommandState.kt
+++ src/main/kotlin/pl/dwojciechowski/execution/state/RemoteCommandState.kt
@@ -7,6 +7,10 @@ import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.process.NopProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.wm.ToolWindowManager
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import pl.dwojciechowski.execution.config.RemoteCommandRunConfig
 import pl.dwojciechowski.model.CommandBean
 import pl.dwojciechowski.service.RemoteService
@@ -18,19 +22,22 @@ class RemoteCommandState(private val environment: ExecutionEnvironment) : RunPro
 
     override fun execute(executor: Executor?, runner: ProgramRunner<*>): ExecutionResult? {
         remoteServiceManager.executeStreaming(buildCommandBean())
-//        environment.setCallback {
-//            it.processHandler?.destroyProcess()
-//        }
 
+        return DefaultExecutionResult(object : NopProcessHandler() {
+            override fun startNotify() {
+                super.startNotify()
 
-        return DefaultExecutionResult(object: NopProcessHandler(){
-            override fun isProcessTerminating(): Boolean {
-                return true
-            }
-
-            override fun isProcessTerminated(): Boolean {
-                destroyProcess()
-                return true
+                GlobalScope.launch {
+                    destroyProcess()
+                    val contentManager1 = ToolWindowManager.getInstance(environment.project)
+                        .getToolWindow(executor?.toolWindowId)?.contentManager
+                    contentManager1?.selectedContent?.let {
+                        ApplicationManager.getApplication().invokeLater {
+                            contentManager1.removeContent(it, true)
+                            focusOnPluginTab()
+                        }
+                    }
+                }
             }
         })
     }
@@ -38,6 +45,17 @@ class RemoteCommandState(private val environment: ExecutionEnvironment) : RunPro
     private fun buildCommandBean(): CommandBean {
         val configurationBase = environment.runProfile as RemoteCommandRunConfig
         return CommandBean(configurationBase.settings.command, configurationBase.settings.command, now())
+    }
+
+    private fun focusOnPluginTab() {
+        val instance = ToolWindowManager.getInstance(environment.project)
+        instance.getToolWindow("PLM Companion Log")?.let { toolWindow ->
+            val contentManager = toolWindow.contentManager
+            contentManager.getContent(2)?.let { content ->
+                toolWindow.show()
+                contentManager.setSelectedContent(content, true, false)
+            }
+        }
     }
 
 }
