3e9b4985fb6d29f98f2d8e75361e8ccaeaba3b03
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2020-07-24 16:46:24 +0200

    added base for internationalization

--- src/main/kotlin/pl/dwojciechowski/model/ExecutionStatus.kt
+++ src/main/kotlin/pl/dwojciechowski/model/ExecutionStatus.kt
@@ -1,15 +1,16 @@
 package pl.dwojciechowski.model
 
 import com.intellij.icons.AllIcons
+import jdk.nashorn.internal.runtime.ECMAErrors.getMessage
 import pl.dwojciechowski.ui.PluginIcons
 import javax.swing.Icon
 
 enum class ExecutionStatus(val icon: Icon, val format: String) {
 
-    RUNNING(PluginIcons.RUNNING, "Command %s started."),
-    STOPPED(PluginIcons.ERROR, "Command %s failed."),
-    COMPLETED(PluginIcons.CONFIRMATION, "Command %s finished successfully"),
-    NONE(AllIcons.Debugger.Db_muted_breakpoint, "Command %s created");
+    RUNNING(PluginIcons.RUNNING, getMessage("execution.status.running")),
+    STOPPED(PluginIcons.ERROR, getMessage("execution.status.stopped")),
+    COMPLETED(PluginIcons.CONFIRMATION, "execution.status.completed"),
+    NONE(AllIcons.Debugger.Db_muted_breakpoint, "execution.status.none");
 
     fun getMessage(command: CommandBean) = format.replace("%s", command.toString())
 
