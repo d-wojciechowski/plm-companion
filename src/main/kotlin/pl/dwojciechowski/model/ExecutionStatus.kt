cd99d1140a2dd18063fcd2b034ba7bf7798e75d0
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2020-08-06 13:17:09 +0200

    added missing i18n

--- src/main/kotlin/pl/dwojciechowski/model/ExecutionStatus.kt
+++ src/main/kotlin/pl/dwojciechowski/model/ExecutionStatus.kt
@@ -9,8 +9,8 @@ enum class ExecutionStatus(val icon: Icon, val format: String) {
 
     RUNNING(PluginIcons.RUNNING, getMessage("execution.status.running")),
     STOPPED(PluginIcons.ERROR, getMessage("execution.status.stopped")),
-    COMPLETED(PluginIcons.CONFIRMATION, "execution.status.completed"),
-    NONE(AllIcons.Debugger.Db_muted_breakpoint, "execution.status.none");
+    COMPLETED(PluginIcons.CONFIRMATION, getMessage("execution.status.completed")),
+    NONE(AllIcons.Debugger.Db_muted_breakpoint, getMessage("execution.status.none"));
 
     fun getMessage(command: CommandBean) = format.replace("%s", command.toString())
 
