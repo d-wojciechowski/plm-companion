f37cab63f18efde15ad3d89b3fa61738425385a5
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2021-07-28 08:02:18 +0200

    removed warnings.

--- src/main/kotlin/pl/dwojciechowski/service/impl/NotificationServiceImpl.kt
+++ src/main/kotlin/pl/dwojciechowski/service/impl/NotificationServiceImpl.kt
@@ -1,9 +1,6 @@
 package pl.dwojciechowski.service.impl
 
-import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.project.Project
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import pl.dwojciechowski.model.ServerStatus
 import pl.dwojciechowski.service.NotificationService
 import pl.dwojciechowski.service.StatusService
@@ -11,21 +8,19 @@ import pl.dwojciechowski.ui.PLMPluginNotification
 
 class NotificationServiceImpl(project: Project) : NotificationService {
 
-    private val statusService = ServiceManager.getService(project, StatusService::class.java)
+    private val statusService = project.getService(StatusService::class.java)
 
     private var previousStatus = ServerStatus.DOWN
 
     init {
-        GlobalScope.launch {
-            statusService.getOutputSubject().subscribe { status ->
-                when (status) {
-                    previousStatus -> Unit
-                    ServerStatus.AVAILABLE -> PLMPluginNotification.apacheOK(project)
-                    ServerStatus.RUNNING -> PLMPluginNotification.serverOK(project)
-                    else -> PLMPluginNotification.serverKO(project)
-                }
-                previousStatus = status
+        statusService.getOutputSubject().subscribe { status ->
+            when (status) {
+                previousStatus -> Unit
+                ServerStatus.AVAILABLE -> PLMPluginNotification.apacheOK(project)
+                ServerStatus.RUNNING -> PLMPluginNotification.serverOK(project)
+                else -> PLMPluginNotification.serverKO(project)
             }
+            previousStatus = status
         }
     }
 
